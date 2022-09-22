46f09d47a7419790f69e7159aba37f3abd3ed0d3
t-tornado <anthonyamp0011@gmail.com>
2022-09-22 14:27:06 +0000

    fix

--- src/components/app/album.container.tsx
+++ src/components/app/album.container.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export const AlbumContainer: React.FC<Props> = (props) => {
   return (
-    <div className="relative overflow-hidden w-72 h-16 rounded-md bg-white flex items-center justify-center">
+    <div className="relative overflow-hidden w-72 h-16 p-2 rounded-md bg-white flex items-center justify-center">
       <p className="font-poppins text-sm font-bold">{props.title}</p>
       {!props?.visible && (
         <div className="absolute flex items-center justify-center h-full w-full bg-[#00000040] backdrop-blur-sm">
--- src/components/app/app.background.tsx
+++ src/components/app/app.background.tsx
@@ -8,7 +8,7 @@ interface Props {
 export const AppBackground: React.FC<Props> = (props) => {
   return (
     <div
-      className={`h-screen w-screen bg-[url('./assets/background.jpg')] overflow-hidden`}
+      className={`h-screen w-screen bg-[url('./assets/background.jpg')] overflow-x-hidden overflow-y-auto`}
     >
       <div
         className={`h-full w-full backdrop-blur-3xl bg-[#00000050]  ${props?.className}`}
